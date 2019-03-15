21dc8fbb80ab0bca7982ddcf42ab96a8cac823e3
fitfingers <hooper.j.1991@gmail.com>
2019-03-15 12:39:05 +0100

    Must edit index

--- App.js
+++ App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function Title() {
+  return (
+    <div>
+      <h1>Don't Forget:</h1>
+    </div>
+  );
 }
+
+// function MainCont() {
+//   return (
+//     <div>
+//       <InputBox />
+      
+//     </div>
+//   );
+// }
+
+function InputBox() {
+  const [input, setInput] = useState("");
+}
+
+function List(props) {
+  return (
+    <div>
+      {this.props.list.map(i => <div><p>{i}</p></div>)}
+    </div>
+  );
+}
+
+function Options(props) {
+  return (
+    <div>
+      <p>Num i left</p>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+      <button>Clear completed</button>
+    </div>
+  );
+}
+
+// function ListItem() {
+//   const 
+// }
+
+const App = () => {
+  const [list, setList] = useState([]);
+  return (
+    <div className="App">
+      
+      <Title />
+
+      <div id="mainCont">
+        <InputBox />
+        <List />
+        <Options />
+      </div>
+
+    </div>
+  );
+};
 
 export default App;
